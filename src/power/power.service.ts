b58e36556c35e5df6efcdd2ecb81f5f59ce9e038
Debangshu Dey <deydebangshu51@gmail.com>
2026-02-28 11:04:43 +0530

    connect power to disk and connect computer with disk and cpu

--- src/power/power.service.ts
+++ src/power/power.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class PowerService {
   supplyPower(watts: number) {
-    console.log(`supplying ${watts} worth if power`);
+    console.log(`supplying ${watts} worth of power`);
   }
 }
